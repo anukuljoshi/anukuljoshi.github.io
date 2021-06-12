a25ef3e31f33606e7ee1f257e5925fc50f6121e2
Anukul Joshi <anukuljoshi619@gmail.com>
2021-06-12 19:06:18 +0530

    purges tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,55 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      colors: {
-        backdrop: '#252a34',
-        foreground: '#112240',
-        bluebg: '#0A192F',
-        primary: '#ff2e63',
-        secondary: '#08d9d6',
-        highlight: '#64FFDA',
-        text: '#eaeaea'
-      },
-      fontFamily: {
-        'aleg': ['Alegreya', 'Helvetica', 'Verdana', 'Monospace', 'Arial'],
-        'cairo': ['Cairo', 'Helvetica', 'Verdana', 'Monospace', 'Arial'],
-        'rubik': ['Rubik', 'Helvetica', 'Verdana', 'Monospace', 'Arial'],
-        'titil': ['Titillium Web', 'Helvetica', 'Verdana', 'Monospace', 'Arial'],
-        'concert': ['Concert One', 'Helvetica', 'Verdana', 'Monospace', 'Arial'],
-      }
-    },
-  },
-  variants: {
-    extend: {
-      fontSize: ['hover', 'focus']
-    },
-  },
-  plugins: [],
-}
+	purge: {
+		enabled: true,
+		content: [
+			'./src/**/*.jsx',
+			'./src/**/*.js',
+			'./public/index.html'
+		]
+	},
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: {
+				backdrop: "#252a34",
+				foreground: "#112240",
+				bluebg: "#0A192F",
+				primary: "#ff2e63",
+				secondary: "#08d9d6",
+				highlight: "#64FFDA",
+				text: "#eaeaea",
+			},
+			fontFamily: {
+				aleg: [
+					"Alegreya",
+					"Helvetica",
+					"Verdana",
+					"Monospace",
+					"Arial",
+				],
+				cairo: ["Cairo", "Helvetica", "Verdana", "Monospace", "Arial"],
+				rubik: ["Rubik", "Helvetica", "Verdana", "Monospace", "Arial"],
+				titil: [
+					"Titillium Web",
+					"Helvetica",
+					"Verdana",
+					"Monospace",
+					"Arial",
+				],
+				concert: [
+					"Concert One",
+					"Helvetica",
+					"Verdana",
+					"Monospace",
+					"Arial",
+				],
+			},
+		},
+	},
+	variants: {
+		extend: {
+			fontSize: ["hover", "focus"],
+		},
+	},
+	plugins: [],
+};
